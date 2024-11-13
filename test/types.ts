0797f1ae1ac0dc5c96958f7e874b23184912bef3
Aurora Poppyseed <30662672+poppyseedDev@users.noreply.github.com>
2024-11-13 21:18:38 +0900

    Initialize async/erc20

---
+++ test/types.ts
@@ -0,0 +1,20 @@
+import type { FhevmInstance } from "fhevmjs";
+
+import { EncryptedERC20 } from "../types";
+import type { Signers } from "./signers";
+
+declare module "mocha" {
+  export interface Context {
+    signers: Signers;
+    contractAddress: string;
+    instances: FhevmInstances;
+  }
+}
+
+export interface FhevmInstances {
+  alice: FhevmInstance;
+  bob: FhevmInstance;
+  carol: FhevmInstance;
+  dave: FhevmInstance;
+  eve: FhevmInstance;
+}
